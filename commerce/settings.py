84c9675d887563af96da8ee6103d62e0f76f4331
muvikt <muvikt@gmail.com>
2015-07-01 14:40:46 +0200

    First commit

---
+++ commerce/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for commerce project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'commerce'
+
+SPIDER_MODULES = ['commerce.spiders']
+NEWSPIDER_MODULE = 'commerce.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'commerce (+http://www.yourdomain.com)'
